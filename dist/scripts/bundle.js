dd7f43a92a348feb3fa39576dc72f6b959b3babe
seanD <sean@drivelous.com>
2016-07-16 16:05:07 -0400

    React Forms/Build Controller View

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -48468,22 +48468,19 @@ module.exports = {
 var React = require('react');
 
 var About = React.createClass({displayName: "About",
-    statics: {
-        willTransitionTo: function(transition, params, query, callback) {
-            if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
-                transition.about();
-            } else {
-                callback();
-            }
-        },
-        willTransitionFrom: function(transition, component) {
-            if (!confirm('Are you sure you want to leave this boring page?')) {
-                transition.about();
-            } else {
-                callback();
-            }
-        }
-    },
+    // statics: {
+    //     willTransitionTo: function(transition, params, query, callback) {
+    //         if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+    //             transition.about();
+    //         }
+    //     },
+
+    //     willTransitionFrom: function(transition, component) {
+    //         if (!confirm('Are you sure you want to leave this boring page?')) {
+    //             transition.about();
+    //         }
+    //     }
+    // },
 
     render: function() {
         return (
@@ -48531,7 +48528,7 @@ var App = React.createClass({displayName: "App",
 
 module.exports = App;
 
-},{"./common/header":204,"jquery":2,"react":197,"react-router":28}],202:[function(require,module,exports){
+},{"./common/header":205,"jquery":2,"react":197,"react-router":28}],202:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -48572,6 +48569,8 @@ module.exports = AuthorList;
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = require('react-router').Link;
 var AuthorApi = require('../../api/authorApi');
 var AuthorList = require('./authorList');
 
@@ -48592,6 +48591,7 @@ var AuthorPage = React.createClass({displayName: "AuthorPage",
         return (
             React.createElement("div", null, 
                 React.createElement("h1", null, "Authors"), 
+                React.createElement(Link, {to: "addAuthor", className: "btn btn-default"}, "Add Author"), 
                 React.createElement(AuthorList, {authors: this.state.authors})
             )
         );
@@ -48600,7 +48600,20 @@ var AuthorPage = React.createClass({displayName: "AuthorPage",
 
 module.exports = AuthorPage;
 
-},{"../../api/authorApi":198,"./authorList":202,"react":197}],204:[function(require,module,exports){
+},{"../../api/authorApi":198,"./authorList":202,"react":197,"react-router":28}],204:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+
+var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
+    render: function() {
+        return (
+            React.createElement("h1", null, "Manage Author")
+        );
+    }
+})
+
+},{"react":197}],205:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -48628,7 +48641,7 @@ var Header = React.createClass({displayName: "Header",
 
 module.exports = Header;
 
-},{"react":197,"react-router":28}],205:[function(require,module,exports){
+},{"react":197,"react-router":28}],206:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -48649,7 +48662,7 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":197,"react-router":28}],206:[function(require,module,exports){
+},{"react":197,"react-router":28}],207:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -48669,7 +48682,7 @@ var NotFoundPage = React.createClass({displayName: "NotFoundPage",
 
 module.exports = NotFoundPage;
 
-},{"react":197,"react-router":28}],207:[function(require,module,exports){
+},{"react":197,"react-router":28}],208:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -48680,7 +48693,7 @@ Router.run(routes, function(Handler) {
     React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
 
-},{"./routes":208,"react":197,"react-router":28}],208:[function(require,module,exports){
+},{"./routes":209,"react":197,"react-router":28}],209:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -48695,6 +48708,7 @@ var routes = (
     React.createElement(Route, {name: "app", path: "/", handler: require('./components/app')}, 
         React.createElement(DefaultRoute, {handler: require('./components/homePage')}), 
         React.createElement(Route, {name: "authors", handler: require('./components/authors/authorPage')}), 
+        React.createElement(Route, {name: "addAuthor", path: "author", handler: require('./components/authors/manageAuthorPage.js')}), 
         React.createElement(Route, {name: "about", handler: require('./components/about/aboutPage')}), 
         React.createElement(NotFoundRoute, {handler: require('./components/notFoundPage')}), 
         React.createElement(Redirect, {from: "about-us", to: "about"}), 
@@ -48705,4 +48719,4 @@ var routes = (
 
 module.exports = routes;
 
-},{"./components/about/aboutPage":200,"./components/app":201,"./components/authors/authorPage":203,"./components/homePage":205,"./components/notFoundPage":206,"react":197,"react-router":28}]},{},[207]);
+},{"./components/about/aboutPage":200,"./components/app":201,"./components/authors/authorPage":203,"./components/authors/manageAuthorPage.js":204,"./components/homePage":206,"./components/notFoundPage":207,"react":197,"react-router":28}]},{},[208]);
--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,20 +3,19 @@
 var React = require('react');
 
 var About = React.createClass({
-    statics: {
-        willTransitionTo: function(transition, params, query, callback) {
-            if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
-                transition.about();
-            } else {
-                callback();
-            }
-        },
+    // statics: {
+    //     willTransitionTo: function(transition, params, query, callback) {
+    //         if (!confirm('Are you sure you want to read a page that\'s this boring?')) {
+    //             transition.about();
+    //         }
+    //     },
 
-        willTransitionFrom: function(transition, component) {
-            if (!confirm('Are you sure you want to leave this boring page?')) {
-                transition.about();
-        }
-    },
+    //     willTransitionFrom: function(transition, component) {
+    //         if (!confirm('Are you sure you want to leave this boring page?')) {
+    //             transition.about();
+    //         }
+    //     }
+    // },
 
     render: function() {
         return (
