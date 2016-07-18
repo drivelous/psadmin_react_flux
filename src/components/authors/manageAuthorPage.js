f06334ac1394469b2350c0f4acee5c084c975b3b
seanD <sean@drivelous.com>
2016-07-17 22:22:30 -0400

    Beginning Flux Demos: Stores: Initialization

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -3,6 +3,8 @@
 var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
+var AuthorActions = require('../../actions/authorActions');
+var AuthorStore = require('../../stores/authorStore');
 var AuthorApi = require('../../api/authorApi');
 var toastr = require('toastr');
 
@@ -11,14 +13,32 @@ var ManageAuthorPage = React.createClass({
         Router.Navigation
     ],
 
+    statics: {
+        willTransitionFrom: function(transition, component) {
+            if (component.state.dirty && !confirm('Leave without saving?')) {
+                transition.abort();
+            }
+        }
+    },
+
     getInitialState: function() {
         return {
             author: { id: '', firstName: '', lastName: '' },
-            errors: {}
+            errors: {},
+            dirty: false
         };
     },
 
+    componentWillMount: function() {
+        var authorId = this.props.params.id; // from the path '/author:id'
+
+        if (authorId) {
+            this.setState({ author: AuthorApi.getAuthorById(authorId)});
+        }
+    },
+
     setAuthorState: function(event) {
+        this.setState({ dirty: true });
         var field = event.target.name;
         var value = event.target.value;
         this.state.author[field] = value;
@@ -50,7 +70,9 @@ var ManageAuthorPage = React.createClass({
             return;
         }
 
-        AuthorApi.saveAuthor(this.state.author);
+        // AuthorApi.saveAuthor(this.state.author);
+        AuthorActions.createAuthor(this.state.author);
+        this.setState({ dirty: false });
         toastr.success('Author saved');
         this.transitionTo('authors');
     },
